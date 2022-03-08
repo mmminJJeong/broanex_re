66839207c1af87d98fcc8f2f777f78446ce0e9a9
mmminJJeong <flzn0100@gmail.com>
2022-03-08 18:26:28 +0900

    Revert "서버연결 구동 완료"
    This reverts commit da7bf768c865cf5cea915938a42a9a74c6d4e258.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Popup from "./components/popup";
 import "./public.css";
 import Router from "./router/router";
 
 function App() {
   return (
     <>
-      <Popup />
       <Router />
     </>
   );
